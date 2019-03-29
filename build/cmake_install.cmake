dd0b3c6fafddfb6028abb02c2ff2989ddf15ecdc
dongmuliu <983860513@qq.com>
2019-03-29 21:03:53 +0800

    增加ubuntu可读CMakelists文件

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/testKinect1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/TestOpenNI")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
